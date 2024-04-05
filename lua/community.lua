82ff35cb5866c04291ed8d500c51a8b0f7269042
Ivan D Vasin <ivan@nisavid.io>
2024-04-05 14:30:32 -0400

    refactor(yanky.nvim): use community plugin
    The fix for <https://github.com/AstroNvim/astrocommunity/issues/830>
    has now been merged upstream.

--- lua/community.lua
+++ lua/community.lua
@@ -122,73 +122,14 @@ return {
       require("telescope").load_extension "undo"
     end,
   },
-  -- TODO: After <https://github.com/AstroNvim/astrocommunity/issues/830> is resolved
-  --{ import = "astrocommunity.editing-support.yanky-nvim" },
-  --{
-  --  "gbprod/yanky.nvim",
-  --  opts = { ring = { history_length = 100 } },
-  --},
-  --{
-  --  "nvim-telescope/telescope.nvim",
-  --  keys = {
-  --    { "<Leader>fy", function() require("telescope").extensions.yank_history.yank_history() end, desc = "Find yanks" },
-  --  },
-  --  opts = { extensions = { yank_history = { dynamic_preview_title = true } } },
-  --},
+  { import = "astrocommunity.editing-support.yanky-nvim" },
   {
     "gbprod/yanky.nvim",
-    dependencies = {
-      { "kkharji/sqlite.lua", enabled = not jit.os:find "Windows" },
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            n = {
-              ["<Leader>fy"] = { "<Cmd>Telescope yank_history<CR>", desc = "Open Yank History" },
-              ["y"] = { "<Plug>(YankyYank)", desc = "Yank text" },
-              ["p"] = { "<Plug>(YankyPutAfter)", desc = "Put yanked text after cursor" },
-              ["P"] = { "<Plug>(YankyPutBefore)", desc = "Put yanked text before cursor" },
-              ["gp"] = { "<Plug>(YankyGPutAfter)", desc = "Put yanked text after selection" },
-              ["gP"] = { "<Plug>(YankyGPutBefore)", desc = "Put yanked text before selection" },
-              ["[y"] = { "<Plug>(YankyCycleForward)", desc = "Cycle forward through yank history" },
-              ["]y"] = { "<Plug>(YankyCycleBackward)", desc = "Cycle backward through yank history" },
-              ["]p"] = { "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-              ["[p"] = { "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-              ["]P"] = { "<Plug>(YankyPutIndentAfterLinewise)", desc = "Put indented after cursor (linewise)" },
-              ["[P"] = { "<Plug>(YankyPutIndentBeforeLinewise)", desc = "Put indented before cursor (linewise)" },
-              [">p"] = { "<Plug>(YankyPutIndentAfterShiftRight)", desc = "Put and indent right" },
-              ["<p"] = { "<Plug>(YankyPutIndentAfterShiftLeft)", desc = "Put and indent left" },
-              [">P"] = { "<Plug>(YankyPutIndentBeforeShiftRight)", desc = "Put before and indent right" },
-              ["<P"] = { "<Plug>(YankyPutIndentBeforeShiftLeft)", desc = "Put before and indent left" },
-              ["=p"] = { "<Plug>(YankyPutAfterFilter)", desc = "Put after applying a filter" },
-              ["=P"] = { "<Plug>(YankyPutBeforeFilter)", desc = "Put before applying a filter" },
-            },
-            x = {
-              ["y"] = { "<Plug>(YankyYank)", desc = "Yank text" },
-              ["p"] = { "<Plug>(YankyPutAfter)", desc = "Put yanked text after cursor" },
-              ["P"] = { "<Plug>(YankyPutBefore)", desc = "Put yanked text before cursor" },
-              ["gp"] = { "<Plug>(YankyGPutAfter)", desc = "Put yanked text after selection" },
-              ["gP"] = { "<Plug>(YankyGPutBefore)", desc = "Put yanked text before selection" },
-            },
-          },
-        },
-      },
-    },
-    opts = function()
-      local mapping = require "yanky.telescope.mapping"
-      local mappings = mapping.get_defaults()
-      mappings.i["<c-p>"] = nil
-      return {
-        highlight = { timer = 200 },
-        ring = { storage = jit.os:find "Windows" and "shada" or "sqlite" },
-        picker = {
-          telescope = {
-            use_default_mappings = false,
-            mappings = mappings,
-          },
-        },
-      }
-    end,
+    opts = { ring = { history_length = 100 } },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = { extensions = { yank_history = { dynamic_preview_title = true } } },
   },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
