0017e4c0d7eefd7ebc47887df6cf26d98b123117
Ivan D Vasin <ivan@nisavid.io>
2024-04-05 14:20:29 -0400

    feat(community/programming-language-support): add nvim-jqx, rest-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -236,6 +236,8 @@ return {
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.zig" },
   { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.programming-language-support.nvim-jqx" },
+  { import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.programming-language-support.dooku-nvim" },
   { import = "astrocommunity.programming-language-support.web-tools-nvim" },
   { import = "astrocommunity.project.nvim-spectre" },
